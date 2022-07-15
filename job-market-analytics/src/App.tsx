95b1fc18d86eb71dfc9f1712aa943097b06acd5a
Petra Carrion <petra@carrion.io>
2022-07-15 20:19:01 +0200

    Edit App.tsx

--- job-market-analytics/src/App.tsx
+++ job-market-analytics/src/App.tsx
@@ -11,9 +11,39 @@ const filterItems: FilterContainerProps = {
                     name: 'Vodafone',
                     weight: '99',
                     checked: false,
-                }
+                },
+                {
+                    name: 'O2',
+                    weight: '99',
+                    checked: false,
+                },
+                {
+                    name: 'Telekom',
+                    weight: '99',
+                    checked: false,
+                },
             ],
-        }
+        },
+        {
+            name: 'city',
+            options: [
+                {
+                    name: 'Berlin',
+                    weight: '99',
+                    checked: false,
+                },
+                {
+                    name: 'Hamburg',
+                    weight: '99',
+                    checked: false,
+                },
+                {
+                    name: 'Heilbronn',
+                    weight: '99',
+                    checked: false,
+                },
+            ],
+        },
     ],
 };
 
